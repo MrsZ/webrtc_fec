cc73ab84126508a07684c4ef78f3dc545bc4d0f1
wisherzhang <659539109@qq.com>
2018-05-10 19:17:17 +0800

    build ok for encoder and decoder

---
+++ webrtc_fec/test-flexfec-encoder/main.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+
+
+#include "modules/rtp_rtcp/include/flexfec_sender.h"
+#include "modules/rtp_rtcp/include/rtp_rtcp_defines.h"
+#include "modules/rtp_rtcp/source/fec_test_helper.h"
+#include "modules/rtp_rtcp/source/rtp_header_extensions.h"
+#include "modules/rtp_rtcp/source/rtp_packet_to_send.h"
+#include "modules/rtp_rtcp/source/rtp_sender.h"
+#include "modules/rtp_rtcp/source/rtp_utility.h"
+namespace webrtc {
+	const std::vector<RtpExtension> kNoRtpHeaderExtensions;
+	const std::vector<RtpExtensionSize> kNoRtpHeaderExtensionSizes;
+	constexpr int64_t kInitialSimulatedClockTime = 1;
+	namespace {
+		void testFlexfecEncoder()
+		{
+			SimulatedClock clock(kInitialSimulatedClockTime);
+			webrtc::FlexfecSender sender(123, 5768, 1234, "",
+				kNoRtpHeaderExtensions, kNoRtpHeaderExtensionSizes,
+				nullptr /* rtp_state */, &clock);
+		}
+	}
+}
+int main()
+{
+	//
+
+	webrtc::testFlexfecEncoder();
+
+	return 0;
+}
